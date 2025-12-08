36b985b34fefae04594cd148b864340fa593062b
Jackie <798265994@qq.com>
2025-12-08 23:35:57 +0800

    feat: 修改细小错误

--- src/main/java/com/example/nexusai/config/MinioConfig.java
+++ src/main/java/com/example/nexusai/config/MinioConfig.java
@@ -6,7 +6,6 @@ import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-i
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
